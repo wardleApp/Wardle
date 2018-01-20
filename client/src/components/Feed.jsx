4e7046f7ab092e3aa9ae130d70caf90d535dce51
jackie <joujackie@gmail.com>
2018-01-19 23:44:08 -0800

    change this to props and change state

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -2,38 +2,38 @@ import React from 'react';
 import FeedTransaction from './FeedTransaction.jsx'
 import {List, ListItem} from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
+import {connect} from 'react-redux';
 
-class Feed extends React.Component {
-  constructor (props) {
-    super(props);
-  }
+const Feed = (props) => {
+  let hasTransactions = props.transactions && props.transactions.items && props.transactions.items.length > 0;
+  let showMoreButton = hasTransactions ? Boolean(props.transactions.nextPageTransactionId) : false;
 
-  render() {
-
-    let hasTransactions = this.props.transactions && this.props.transactions.items && this.props.transactions.items.length > 0;
-    let showMoreButton = hasTransactions ? Boolean(this.props.transactions.nextPageTransactionId) : false;
-
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          {!hasTransactions
-            ? <div> No transactions available. </div>
-            : <List>
-                {this.props.transactions.items && this.props.transactions.items.map((transaction, i) => {
-                  let isLastItem = i === this.props.transactions.items.length - 1;
-                  return (
-                    <FeedTransaction key={transaction.transactionId} isLastItem={isLastItem} transaction={transaction} />
-                  );
-                })}
-              </List>
-          }
-        </div>
-        <div className='show-more'>
-          {showMoreButton && <button className='btn' onClick={ () => this.props.loadMoreFeed(this.props.type, this.props.userId) } >Show More</button>}
-        </div>
+        {!hasTransactions
+          ? <div> No transactions available. </div>
+          : <List>
+              {props.transactions.items && props.transactions.items.map((transaction, i) => {
+                let isLastItem = i === props.transactions.items.length - 1;
+                return (
+                  <FeedTransaction key={transaction.transactionId} isLastItem={isLastItem} transaction={transaction} />
+                );
+              })}
+            </List>
+        }
       </div>
-    );
-  }
+      <div className='show-more'>
+        {showMoreButton && <button className='btn' onClick={ () => props.loadMoreFeed(props.type, props.userId) } >Show More</button>}
+      </div>
+    </div>
+  );
 }
 
-export default Feed;
+const mapStateToProps = (state) => {
+  return {
+    userId: state.userInfo.userId
+  }
+};
+
+export default connect(mapStateToProps)(Feed);
